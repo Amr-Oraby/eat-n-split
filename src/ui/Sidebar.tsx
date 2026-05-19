77f75b8093b2db180d908600f338337dabe33d46
oraby <amrorapy50@gmail.com>
2026-05-19 18:11:27 +0300

    convert sidebar to typescript

---
+++ src/ui/Sidebar.tsx
@@ -0,0 +1,49 @@
+import AddFriendForm from "./AddFriendForm";
+import Button from "./Button";
+import Friend from "./Friend";
+
+type Tfriend = { id: number; name: string; image: string; balance: number };
+
+type SidebarProps = {
+  friends: Tfriend[];
+  selectFriend: Tfriend;
+  showNewFriend: boolean;
+  AddNewFriend(): void;
+  toggleShow(): void;
+  handleToggleSelect(friend: Tfriend): void;
+  deleteFriend(id: number): void;
+};
+
+function Sidebar({
+  friends,
+  AddNewFriend,
+  handleToggleSelect,
+  selectFriend,
+  showNewFriend,
+  toggleShow,
+  deleteFriend,
+}: SidebarProps) {
+  return (
+    <div className="sidebar">
+      <ul>
+        {friends.map((friend) => (
+          <Friend
+            friend={friend}
+            key={friend.id}
+            selectFriend={selectFriend}
+            deleteFriend={deleteFriend}
+            handleToggleSelect={handleToggleSelect}
+          />
+        ))}
+      </ul>
+
+      {showNewFriend && <AddFriendForm AddNewFriend={AddNewFriend} />}
+
+      <Button onClick={toggleShow}>
+        {showNewFriend ? "Close" : "Add Friend"}
+      </Button>
+    </div>
+  );
+}
+
+export default Sidebar;
