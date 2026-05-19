f8cd3e575f3e84747f3ee0f057bac88b42821420
oraby <amrorapy50@gmail.com>
2026-05-19 18:17:37 +0300

    Add  initialFriends

---
+++ src/data/initialFriends.ts
@@ -0,0 +1,20 @@
+export const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=115436",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Jonas",
+    image: "https://i.pravatar.cc/48?u=453472",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
